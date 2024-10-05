a62fe36f71186916adf051f7dbcf91fe55e612b9
SNMetamorph <snmetamorph.adc@gmail.com>
2024-10-05 15:10:42 +0400

    sources: major refactoring in event loop code

--- sources/event_loop.cpp
+++ sources/event_loop.cpp
@@ -22,107 +22,53 @@ GNU General Public License for more details.
 
 struct EventLoop::Impl
 {
-	Impl() {};
+public:
+	Impl(std::shared_ptr<Socket> socketInet, std::shared_ptr<Socket> socketInet6);
 
-	void EventRecvIPv4();
-	void EventRecvIPv6();
-	void EventCleanupTimer();
+	void Run();
+	void RecvInetCallback();
+	void RecvInet6Callback();
+	void CleanupTimerCallback();
 
-	std::unique_ptr<ev::EventBase> eventBase;
-	std::unique_ptr<ev::Event> eventReceiveIPv4;
-	std::unique_ptr<ev::Event> eventReceiveIPv6;
-	std::unique_ptr<ev::Event> eventCleanupTimer;
-	std::unique_ptr<ev::Event> eventSigtermSignal;
-	std::unique_ptr<ev::Event> eventSigintSignal;
+private:
+	void InitInetSocketEvent();
+	void InitInet6SocketEvent();
+	void InitCleanupTimerEvent();
+	void InitSignalsEvents();
 
-	std::shared_ptr<Socket> socketIPv4;
-	std::shared_ptr<Socket> socketIPv6;
-	std::unique_ptr<RequestHandler> packetHandler;
-	std::unique_ptr<ServerList> serverList;
+	std::shared_ptr<Socket> m_socketInet;
+	std::shared_ptr<Socket> m_socketInet6;
+	std::unique_ptr<ServerList> m_serverList;
+	std::unique_ptr<RequestHandler> m_requestHandler;
+	std::unique_ptr<ev::EventBase> m_eventBase;
+	std::unique_ptr<ev::Event> m_receivePacketInetEvent;
+	std::unique_ptr<ev::Event> m_receivePacketInet6Event;
+	std::unique_ptr<ev::Event> m_cleanupTimerEvent;
+	std::unique_ptr<ev::Event> m_sigtermSignalEvent;
+	std::unique_ptr<ev::Event> m_sigintSignalEvent;
 };
 
-EventLoop::EventLoop(std::shared_ptr<Socket> socketIPv4, std::shared_ptr<Socket> socketIPv6)
+EventLoop::Impl::Impl(std::shared_ptr<Socket> socketInet, std::shared_ptr<Socket> socketInet6) :
+	m_socketInet(socketInet),
+	m_socketInet6(socketInet6),
+	m_serverList(std::make_unique<ServerList>()),
+	m_requestHandler(std::make_unique<RequestHandler>(*m_serverList)),
+	m_eventBase(std::make_unique<ev::EventBase>())
 {
-	m_impl = std::make_unique<Impl>();
-	m_impl->socketIPv4 = socketIPv4;
-	m_impl->socketIPv6 = socketIPv6;
-	m_impl->serverList = std::make_unique<ServerList>();
-	m_impl->packetHandler = std::make_unique<RequestHandler>(*m_impl->serverList);
-	m_impl->eventBase = std::make_unique<ev::EventBase>();
 	evutil_secure_rng_init();
-
-	if (m_impl->socketIPv4)
-	{
-		auto ipv4RecvCallback = [](evutil_socket_t fd, short event, void *arg) {
-			EventLoop *eventLoop = reinterpret_cast<EventLoop*>(arg);
-			eventLoop->m_impl->EventRecvIPv4();
-		};
-
-		m_impl->eventReceiveIPv4 = std::make_unique<ev::Event>(
-			*m_impl->eventBase, 
-			m_impl->socketIPv4->GetDescriptor(),
-			EV_READ | EV_PERSIST,
-			ipv4RecvCallback,
-			this
-		);
-		m_impl->eventReceiveIPv4->Add();
+	if (socketInet) {
+		InitInetSocketEvent();
 	}
-
-	if (m_impl->socketIPv6)
-	{
-		auto ipv6RecvCallback = [](evutil_socket_t fd, short event, void *arg) {
-			EventLoop *eventLoop = reinterpret_cast<EventLoop*>(arg);
-			eventLoop->m_impl->EventRecvIPv6();
-		};
-
-		m_impl->eventReceiveIPv6 = std::make_unique<ev::Event>(
-			*m_impl->eventBase,
-			m_impl->socketIPv6->GetDescriptor(),
-			EV_READ | EV_PERSIST,
-			ipv6RecvCallback,
-			this
-		);
-		m_impl->eventReceiveIPv6->Add();
+	if (socketInet6) {
+		InitInet6SocketEvent();
 	}
+	InitCleanupTimerEvent();
+	InitSignalsEvents();
+}
 
-	auto cleanupTimerCallback = [](evutil_socket_t fd, short event, void *arg) {
-		EventLoop *eventLoop = reinterpret_cast<EventLoop*>(arg);
-		eventLoop->m_impl->EventCleanupTimer();
-	};
-
-	timeval timerInterval = { 10, 0 };
-	m_impl->eventCleanupTimer = std::make_unique<ev::Event>(
-		*m_impl->eventBase, 
-		-1, 
-		EV_PERSIST, 
-		cleanupTimerCallback, 
-		this
-	);
-	m_impl->eventCleanupTimer->Add(&timerInterval);
-
-	auto signalCallback = [](evutil_socket_t fd, short event, void *arg) {
-		EventLoop *eventLoop = reinterpret_cast<EventLoop*>(arg);
-		eventLoop->m_impl->eventBase->LoopExit();
-	};
-
-	m_impl->eventSigtermSignal = std::make_unique<ev::Event>(
-		*m_impl->eventBase, 
-		SIGTERM, 
-		EV_PERSIST | EV_SIGNAL, 
-		signalCallback, 
-		this
-	);
-
-	m_impl->eventSigintSignal = std::make_unique<ev::Event>(
-		*m_impl->eventBase, 
-		SIGINT, 
-		EV_PERSIST | EV_SIGNAL, 
-		signalCallback, 
-		this
-	);
-
-	m_impl->eventSigtermSignal->Add();
-	m_impl->eventSigintSignal->Add();
+EventLoop::EventLoop(std::shared_ptr<Socket> socketInet, std::shared_ptr<Socket> socketInet6)
+{
+	m_impl = std::make_unique<Impl>(socketInet, socketInet6);
 }
 
 EventLoop::~EventLoop()
@@ -131,22 +77,106 @@ EventLoop::~EventLoop()
 
 void EventLoop::Run()
 {
-	m_impl->eventBase->Dispatch();
+	m_impl->Run();
 }
 
-void EventLoop::Impl::EventRecvIPv4()
+void EventLoop::Impl::Run()
 {
-	NetAddress senderAddr = socketIPv4->RecvFrom();
-	packetHandler->HandlePacket(*socketIPv4, senderAddr);
+	m_eventBase->Dispatch();
 }
 
-void EventLoop::Impl::EventRecvIPv6()
+void EventLoop::Impl::InitInetSocketEvent()
 {
-	NetAddress senderAddr = socketIPv6->RecvFrom();
-	packetHandler->HandlePacket(*socketIPv6, senderAddr);
+	auto inetRecvCallback = [](evutil_socket_t fd, short event, void *arg) {
+		EventLoop::Impl *impl = reinterpret_cast<EventLoop::Impl*>(arg);
+		impl->RecvInetCallback();
+	};
+
+	m_receivePacketInetEvent = std::make_unique<ev::Event>(
+		*m_eventBase, 
+		m_socketInet->GetDescriptor(),
+		EV_READ | EV_PERSIST,
+		inetRecvCallback,
+		this
+	);
+	m_receivePacketInetEvent->Add();
 }
 
-void EventLoop::Impl::EventCleanupTimer()
+void EventLoop::Impl::InitInet6SocketEvent()
 {
-	serverList->CleanupStallServers();
+	auto inet6RecvCallback = [](evutil_socket_t fd, short event, void *arg) {
+		EventLoop::Impl *impl = reinterpret_cast<EventLoop::Impl*>(arg);
+		impl->RecvInet6Callback();
+	};
+
+	m_receivePacketInet6Event = std::make_unique<ev::Event>(
+		*m_eventBase,
+		m_socketInet6->GetDescriptor(),
+		EV_READ | EV_PERSIST,
+		inet6RecvCallback,
+		this
+	);
+	m_receivePacketInet6Event->Add();
+}
+
+void EventLoop::Impl::InitCleanupTimerEvent()
+{
+	auto cleanupTimerCallback = [](evutil_socket_t fd, short event, void *arg) {
+		EventLoop::Impl *impl = reinterpret_cast<EventLoop::Impl*>(arg);
+		impl->CleanupTimerCallback();
+	};
+
+	timeval timerInterval = { 10, 0 };
+	m_cleanupTimerEvent = std::make_unique<ev::Event>(
+		*m_eventBase, 
+		-1, 
+		EV_PERSIST, 
+		cleanupTimerCallback, 
+		this
+	);
+	m_cleanupTimerEvent->Add(&timerInterval);
+}
+
+void EventLoop::Impl::InitSignalsEvents()
+{
+	auto signalCallback = [](evutil_socket_t fd, short event, void *arg) {
+		EventLoop::Impl *impl = reinterpret_cast<EventLoop::Impl*>(arg);
+		impl->m_eventBase->LoopExit();
+	};
+
+	m_sigtermSignalEvent = std::make_unique<ev::Event>(
+		*m_eventBase, 
+		SIGTERM, 
+		EV_PERSIST | EV_SIGNAL, 
+		signalCallback, 
+		this
+	);
+
+	m_sigintSignalEvent = std::make_unique<ev::Event>(
+		*m_eventBase, 
+		SIGINT, 
+		EV_PERSIST | EV_SIGNAL, 
+		signalCallback, 
+		this
+	);
+
+	m_sigtermSignalEvent->Add();
+	m_sigintSignalEvent->Add();
+}
+
+void EventLoop::Impl::RecvInetCallback()
+{
+	NetAddress senderAddr = m_socketInet->RecvFrom();
+	m_requestHandler->HandlePacket(*m_socketInet, senderAddr);
+}
+
+void EventLoop::Impl::RecvInet6Callback()
+{
+	NetAddress senderAddr = m_socketInet6->RecvFrom();
+	m_requestHandler->HandlePacket(*m_socketInet6, senderAddr);
+}
+
+void EventLoop::Impl::CleanupTimerCallback()
+{
+	m_serverList->CleanupStallServers();
 }
